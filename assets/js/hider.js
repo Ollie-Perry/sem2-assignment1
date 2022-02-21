724ac50c96ae00c460f9f804435c49da3bde91b9
Ollie-Perry <oliver2.perry@live.uwe.ac.uk>
2022-02-21 12:25:32 +0000

    reservations

---
+++ assets/js/hider.js
@@ -0,0 +1,17 @@
+function hideDivOne() {
+    var x = document.getElementById("laundrette-form");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hideDivTwo() {
+    var x = document.getElementById("tables-form");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
